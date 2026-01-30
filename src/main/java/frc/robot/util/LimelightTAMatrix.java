ba551f7af4add5bf44085476e0b37177297783db
Darwin <forddarwin6@gmail.com>
2026-01-29 19:15:45 -0600

    Update LimelightTAMatrix.java

--- src/main/java/frc/robot/util/LimelightTAMatrix.java
+++ src/main/java/frc/robot/util/LimelightTAMatrix.java
@@ -12,7 +12,7 @@ public class LimelightTAMatrix {
         taMap.put(4.5, 75.0);
         taMap.put(2.55, 100.0);
         taMap.put(1.7, 125.0);
-        taMap.put(1.18, 150.0); // this is about the edge of its vision, always can improve quality of the vision by adding more values / keys
+        taMap.put(1.18, 150.0);
     }
 
     public static double get(double ta) {
