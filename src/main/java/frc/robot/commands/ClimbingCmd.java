d62a3f1d162f95ecfd27cde7bb29ec5ea428aa0d
Devin Kovacs <deviweviwoohoo@gmail.com>
2026-02-07 14:30:58 -0600

    finally fixed the issues being experienced with climb command -DK

---
+++ src/main/java/frc/robot/commands/ClimbingCmd.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Climber;
+
+public class ClimbingCmd extends Command {
+    private Climber climb;
+    private double direction;
+
+    public ClimbingCmd(Climber subsystem) { // Sets everything up
+        this.climb = subsystem;
+        addRequirements(subsystem);
+    }
+
+    @Override//if at the bottom .2, if at the top -.2 if in the middle keeps moving
+    public void initialize() {
+        if (climb.getHeight()!=1){
+            direction=-(climb.getHeight()-1)/5;
+        }
+    }
+
+    @Override
+    public void execute() {
+        climb.climb(direction);
+    }
+    @Override//if it's at the right point
+    public boolean isFinished() {
+        return climb.getHeight()-1==direction*5;
+    }
+    @Override
+    public void end(boolean interrupted) {
+        climb.climb(0);
+    }
+}
